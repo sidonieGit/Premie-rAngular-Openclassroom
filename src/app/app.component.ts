9d8db8ebfd2bea9201d2506d356decbaec7235ba
sidonieGit <sifohouo@gmail.com>
2024-07-07 11:26:43 +0200

    ng g c landing-page modif de app-component remplace de app'list par routeroutler creation de tableau de route dans app.routes modif de angular.json en ajoutant output assets dans assets car snaplogo plutot dans public et dossier asset absent en realite ajout de snapface-logo dans dossier public

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FaceSnapComponent } from './face-snap/face-snap.component';
-import { CommonModule } from '@angular/common';
-import { FaceSnap } from './models/face-snap';
-import { FaceSnapListComponent } from "./face-snap-list/face-snap-list.component";
+import { Component} from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 
 @Component({
@@ -12,8 +9,8 @@ import { HeaderComponent } from './header/header.component';
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
     imports: [
-        FaceSnapListComponent,
-        HeaderComponent
+        HeaderComponent,
+        RouterOutlet   
     ]
 })
 export class AppComponent {
